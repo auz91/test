913e424b963c0e71631ac40ec1277e3d4ed5067d
auz91 <christopher.edward.mcdonald@gmail.com>
2024-04-07 13:45:42 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import './App.css';
+import Employee from './components/Employee';
 
 function App() {
+  console.log('we are about to list all the employees');
+  const showEmployees = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello! Therer
-        
-        </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      { console.log('inside the return')}
+      {showEmployees ? (
+        <>
+        <Employee />
+        <Employee />
+        <Employee />
+        <Employee />
+        <Employee />
+        </>
+      ) : (
+            <p>You cannot see the employees</p>
+  )}
+  </div>
   );
 }
 
